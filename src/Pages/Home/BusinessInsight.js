a422f77cb1e607f28df589983c473a13cf42bed0
Solih Ahmad Sohan <sasohanme@gmail.com>
2022-05-23 18:00:38 +0600

    business insights done

--- src/Pages/Home/BusinessInsight.js
+++ src/Pages/Home/BusinessInsight.js
@@ -2,12 +2,71 @@ import React from "react";
 
 const BusinessInsight = () => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title text-primary">Top Manufacturer !</h2>
-        <p className="text-green-600">More than 2M supplies last month !!</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="card w-96 bg-rose-200 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title text-green-500">Top Manufacturer !</h2>
+          <p className="text-dark-600">More than 2M supplies last month !!</p>
+          <div className="card-actions text-green-600 justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-24 w-24"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm9 4a1 1 0 10-2 0v6a1 1 0 102 0V7zm-3 2a1 1 0 10-2 0v4a1 1 0 102 0V9zm-3 3a1 1 0 10-2 0v1a1 1 0 102 0v-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="card w-96 bg-rose-200 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title text-green-500">Best in Quality !</h2>
+          <p className="text-dark-600">All Products are of best quality !!</p>
+          <div className="card-actions text-green-600 justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-24 w-24"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="card w-96 bg-rose-200 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title text-green-500">
+            Sincere About Your Time !
+          </h2>
+          <p className="text-dark-600">On Time shipping !!</p>
+          <div className="card-actions text-green-600 justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-24 w-24"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
